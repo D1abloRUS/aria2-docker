9d67ec485fc4c02bc3bea4a8c4658490b50a31f6
D1abloRUS <sergey.monakhov@gmail.com>
2018-02-05 13:43:31 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
-  aria2c --dir=/home --bt-enable-lpd --dht-listen-port=6892 --bt-max-peers=0 --seed-ratio=0.0 --bt-seed-unverified --follow-torrent=mem "${URL}"
+  aria2c --dir=/home --bt-enable-lpd --listen-port=6925 --dht-listen-port=6892 --bt-max-peers=0 --seed-ratio=0.0 --bt-seed-unverified --follow-torrent=mem "${URL}"
 else
   exec "$@"
 fi
